f0ec5f8c931e051effc4b5dc294aa8a3c16bdce7
ar3d <info@ar3d.it>
2022-10-29 17:10:03 +0200

    ok

--- AR/JS/places.js
+++ AR/JS/places.js
@@ -31,53 +31,125 @@ window.onload = () => {
 function staticLoadPlaces() {
     return [
         {
-            name: "Duomo di Larino",
+            name: "L'anfiteatro e la cristianizzazione della <i>Larinum</i> romana",
             location: {
-                lat: 41.801077, // change here latitude if using static data
-                lng: 14.910286, // change here longitude if using static data
+                lat: 41.805512, // change here latitude if using static data
+                lng: 14.916265, // change here longitude if using static data
             },
 			look: "[gps-camera]",
 			image: "assets/map-marker.png",
-			href: "",
-			sfondo: "assets/sfondi/duomo.jpg",
-			text: "La basilica è intitolata a Santa Maria Assunta e San Pardo, patrono della città. La chiesa venne consacrata nel 1319 e prese il posto di un precedente luogo di culto dedicato sempre al santo Vescovo."
+			href: "../start_page/anfiteatro_g.html",
+			sfondo: "assets/sfondi/anfiteatro_G.jpg",
+			text: "La tradizione popolare lega il martirio di Primiano, Firmiano e Casto a questo luogo che rappresenta la testimonianza più significativa del <i>municipium</i> romano di Larino."
         },
 		{
-            name: "Chiesa di San Francesco",
+            name: "Dalla basilica di Sant'Angelo a Palazzo alla cappella della Vergine delle Grazie",
             location: {
-                lat: 41.800840, // change here latitude if using static data
-                lng: 14.910768, // change here longitude if using static data
+                lat: 41.804202, // change here latitude if using static data
+                lng: 14.922016, // change here longitude if using static data
             },
 			look: "[gps-camera]",
 			image: "assets/map-marker.png",
-			href: "",
-			sfondo: "assets/sfondi/san_francesco.jpg",
-			text: "La chiesa e l'annesso convento nascono agli inizi del XIV secolo ad opera dell'ordine francescano. Dell'originaria costruzione rimane poco in quanto nel corso dei secoli l'edificio ha subito profonde modifiche."
+			href: "../start_page/beata_vergine_g.html",
+			sfondo: "assets/sfondi/beata_vergine_G.jpg",
+			text: "La cappella dedicata alla Vergine delle Grazie fu edificata probabilmente sulla basilica dedicata all'arcangelo Michele a cui i longobardi affiancarono la residenza ufficiale dei conti di Larino, prendendo da quel momento il nome di Sant'Angelo a Palazzo."
         },
 		{
-            name: "Palazzo ducale",
+            name: "Il simbolo del potere religioso: l'Episcopio",
             location: {
-                lat: 41.800420, // change here latitude if using static data
-                lng: 14.910763, // change here longitude if using static data
+                lat: 41.800888, // change here latitude if using static data
+                lng: 14.910441, // change here longitude if using static data
             },
 			look: "[gps-camera]",
-			image: "assets/map-marker_2.png",
-			href: "",
-			sfondo: "assets/sfondi/ducale.jpg",
-			text: "Il palazzo in origine era un castello. Fu la famiglia Di Sangro, divenuta feudataria di Larino nel 1683, che trasformò la fortificazione in palazzo gentilizio. Oltre al Comune il palazzo ospita anche il museo civico."
+			image: "assets/map-marker.png",
+			href: "../start_page/episcopio_g.html",
+			sfondo: "assets/sfondi/episcopio_G.jpg",
+			text: "Costruito alla fine del '500 in sostituzione di un più antico Vescovado, le cui tracce si intravedono in una casa di vico Giglio, oggi conserva nelle sue sale la memoria della religiosità larinese che affonda le sue radici nel culto di San Pardo."
         },
 		{
-            name: "Episcopio",
+            name: "La Cattedrale di Santa Maria Assunta e San Pardo",
             location: {
-                lat: 41.800878, // change here latitude if using static data
-                lng: 14.910433, // change here longitude if using static data
+                lat: 41.801088, // change here latitude if using static data
+                lng: 14.910280, // change here longitude if using static data
             },
 			look: "[gps-camera]",
-			image: "assets/map-marker_2.png",
-			href: "",
-			sfondo: "assets/sfondi/episcopio.jpg",
-			text: "Il palazzo ha ospitato il primo seminiario della storia ad opera del vescovo Belisario Balduino nel 1564. Oggi accoglie il MAB, cioè il museo, l'archivio e la bibioteca diocesani."
+			image: "assets/map-marker.png",
+			href: "../start_page/facciata_cattedrale_g.html",
+			sfondo: "assets/sfondi/cattedrale_G.jpg",
+			text: "La chiesa non è solo uno scrigno di arte e storia, ma anche il luogo fisico dove si rinnova il legame tra i larinesi e il loro Patrono."
         },
+		{
+            name: "La fontana di San Pardo e l'arrivo delle reliquie del nuovo Patrono",
+            location: {
+                lat: 41.796791, // change here latitude if using static data
+                lng: 14.917749, // change here longitude if using static data
+            },
+			look: "[gps-camera]",
+			image: "assets/map-marker.png",
+			href: "../start_page/fonte_san_pardo_g.html",
+			sfondo: "assets/sfondi/fonte_G.jpg",
+			text: "La tradizione popolare lega strettamente questa fontana all'arrivo del corpo intatto di San Pardo a Larino nell'842."
+        },
+		{
+            name: "Il simbolo del potere civile: Palazzo ducale",
+            location: {
+                lat: 41.800328, // change here latitude if using static data
+                lng: 14.910854, // change here longitude if using static data
+            },
+			look: "[gps-camera]",
+			image: "assets/map-marker.png",
+			href: "../start_page/palazzo_ducale_g.html",
+			sfondo: "assets/sfondi/palazzo_ducale_G.jpg",
+			text: "Costruito dai Normanni come struttura difensiva, oggi si presenta come un palazzo signorile. Dumas nel romanzo “La Sanfelice” racconta della congiura ai danni del duca Francesco Carafa e dei successivi fatti di sangue ai quali la carrese di San Pardo fa da sfondo."
+        }
+		{
+            name: "San Primiano e la Carrese di San Pardo",
+            location: {
+                lat: 41.804319, // change here latitude if using static data
+                lng: 14.927629, // change here longitude if using static data
+            },
+			look: "[gps-camera]",
+			image: "assets/map-marker.png",
+			href: "../start_page/san_primiano_g.html",
+			sfondo: "assets/sfondi/san_primiano_ricamo_G.jpg",
+			text: "La sera del 25 maggio cominciano i festeggiamenti in onore di San Pardo con il prelievo della statua del copatrono Primiano dalla cappella al centro del cimitero."
+        }
+		{
+            name: "Santa Maria della Pietà e la Laudata di San Pardo",
+            location: {
+                lat: 41.798857, // change here latitude if using static data
+                lng: 14.911741, // change here longitude if using static data
+            },
+			look: "[gps-camera]",
+			image: "assets/map-marker.png",
+			href: "../start_page/santa_maria_del_piano_g.html",
+			sfondo: "assets/sfondi/cs_maria_p_G.jpg",
+			text: "“Ecco, Madonna mia, mo me presento, ti vengo oggi a laudà co suon’e canto.” Così i carrieri iniziavano il canto della Laudata davanti alla chiesa di Santa Maria, legata al culto di san Pardo per il fatto che qui la tradizione vuole sia stato deposto quando arrivò a Larino."
+        }
+		{
+            name: "Verso la chiesa di Santa Maria della Pietà",
+            location: {
+                lat: 41.799886, // change here latitude if using static data
+                lng: 14.911311, // change here longitude if using static data
+            },
+			look: "[gps-camera]",
+			image: "assets/map-marker.png",
+			href: "../start_page/santo_stefano_g.html",
+			sfondo: "assets/sfondi/s_stefano_G.jpg",
+			text: "Siamo all'angolo di quella che oggi è piazza Vittorio Emanuele e che un tempo era lo stazio non edificato antistante il castello. Qui carri, uomini e buoi si radunano nei tre giorni per festeggiare e ringraziare il loro patrono."
+        }
+		{
+            name: "Il primo Seminario dell'Occidente",
+            location: {
+                lat: 41.801381, // change here latitude if using static data
+                lng: 14.910628, // change here longitude if using static data
+            },
+			look: "[gps-camera]",
+			image: "assets/map-marker.png",
+			href: "../start_page/seminario_g.html",
+			sfondo: "assets/sfondi/seminario_G.jpg",
+			text: "Siamo nella parte più antica della Larino medievale, e il passaggio sotto l'arco dell'antico Seminario, il primo inaugurato dopo il Concilio di Trento, è uno dei punti che mette maggiormente alla prova il rapporto tra carrieri e animali."
+        }
     ];
 }
 
