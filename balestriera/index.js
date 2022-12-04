e912762749701fdb81525b629048cc5c2f2855eb
ar3d <info@ar3d.it>
2022-12-04 17:43:22 +0100

    Update index.js

--- balestriera/index.js
+++ balestriera/index.js
@@ -25,7 +25,7 @@ controls.registerMethod('deviceOrientation', deviceOrientationControlMethod);
 
 // Create source.
 var source = Marzipano.ImageUrlSource.fromString(
-  "https://ar3d.github.io/balestriera/media/{f}.jpg"
+  "https://ar3d.github.io/carrese_larino/balestriera/media/{f}.jpg"
 );
 
 // Create geometry.
